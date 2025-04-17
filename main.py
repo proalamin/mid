a91ff95145fd8669f2263c3b2c820c01c7687c63
proalamin <pro.alamin247@gmail.com>
2025-04-17 22:49:45 +0600

    create student database and add student method

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("mid exam......")
+class StudentDatabase:
+    student_list=[]
+
+
+    @classmethod
+    def add_student(cls, student):
+        cls.student_list.append(student)
+
+class Student:
+
+    def __init__(self, student_id, name, department):
+        self.student_id =student_id
+        self.name= name
+        self.department = department
+        self.is_enrolled = False
+
+        StudentDatabase.add_student(self)
+
+s1 = Student(1, "Rahim", "EEE")
+s2 = Student(2, "Karim", "CSE")
+
+
+
+
